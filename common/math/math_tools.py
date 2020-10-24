df209c777a19c2049847c57db03f0ac83087e43d
Yu Fang <squarefk@gmail.com>
2020-10-24 14:53:56 -0400

    fix cofactor

--- common/math/math_tools.py
+++ common/math/math_tools.py
@@ -68,7 +68,7 @@ so = ctypes.CDLL(os.path.dirname(os.path.realpath(__file__)) + "/wrapper/a.so")
 @ti.func
 def cofactor(F):
     if ti.static(F.n == 2):
-        return ti.Matrix([[F[0, 0], -F[1, 0]], [-F[0, 1], F[1, 1]]])
+        return ti.Matrix([[F[1, 1], -F[1, 0]], [-F[0, 1], F[0, 0]]])
     else:
         return ti.Matrix([[F[1, 1] * F[2, 2] - F[1, 2] * F[2, 1], F[1, 2] * F[2, 0] - F[1, 0] * F[2, 2], F[1, 0] * F[2, 1] - F[1, 1] * F[2, 0]],
                           [F[0, 2] * F[2, 1] - F[0, 1] * F[2, 2], F[0, 0] * F[2, 2] - F[0, 2] * F[2, 0], F[0, 1] * F[2, 0] - F[0, 0] * F[2, 1]],
