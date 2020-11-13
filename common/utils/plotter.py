da4f46ac784f05dd3471aa811920a2af8f48d820
Yu Fang <squarefk@gmail.com>
2020-11-13 15:30:20 -0500

    add plotter

---
+++ common/utils/plotter.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pickle
+
+_plotter_data = dict()
+
+
+def Plotter_Record(key, value):
+    if key not in _plotter_data:
+        _plotter_data[key] = []
+    _plotter_data[key].append(value)
+
+
+def Plotter_Dump(path, plot=False, binary=True):
+    for key in _plotter_data:
+        if plot:
+            filename = path + '/' + key + '.png'
+            fig = plt.figure()
+            plt.plot(_plotter_data[key])
+            fig.savefig(filename)
+            plt.close(fig)
+        if binary:
+            pickle.dump(_plotter_data[key], open(path + '/' + key + '.p', 'wb'))
